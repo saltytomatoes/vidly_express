86a2e54fecb99b9841f5727ac049a8d78c7ecac3
saltytomatoes <saltytomatoes1337@gmail.com>
2021-04-20 14:42:35 +0300

    added post request route

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const Joi = require('joi');
 
 
 const app = express();
+const Joi = require('joi');
 const port = process.env.port || 3000;
 
 
@@ -12,22 +12,48 @@ let data = [{ id:0, name: "fast and furious", genre: "action"},
             { id:2, name: "jumanji", genre: "comedy"}];
 
 
+
+
+
+app.use(express.json()); //allows the app to accept json requests.
+
+//get all movies
 app.get('/api/all', (req , res) => {
     res.send(data);
 });
 
+
+//get specific movie.
 app.get('/api/:id', (req , res) => {
     let movie = data.find( movie => movie.id === +req.params.id);
 
-    if(!movie) {
-       res.status(404).send(`sorry! there is no movie with corresponding id: ${req.params.id}`);
-       return; 
-    }
+    if(!movie) return res.status(404).send(`sorry! there is no movie with corresponding id: ${req.params.id}`);
 
     res.send(movie);
 });
 
 
 
+app.post("/api/post", (req , res) => {
+
+    const { error } = validateInput(req.body);
+
+    if(error) return res.status(400).send(error.details[0].message);
+
+    data.push(req.body);
+    res.send(data);
+    
+});
+
+
+
+const validateInput = (movie) => {
+    const schema = {
+        name: Joi.string().required(),
+        genre: Joi.string().required()
+    }
+
+    return Joi.validate(movie, schema);
+}
 
 app.listen(port, () => { console.log(`The server is listening on port ${port}...`) });
